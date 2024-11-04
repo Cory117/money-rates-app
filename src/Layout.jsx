4e43fbcee9fb28a828be4e09fe751d2f1b931305
Cory Daddona <corydaddona1@hotmail.com>
2024-11-04 17:27:16 -0600

    implemented light/dark theme toggle

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,29 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const Layout = (props) => {
+  const [theme, setTheme] = useState(() => {
+    const initialTheme = localStorage.getItem("theme");
+		return initialTheme ? initialTheme : "light";
+  });
+
+  function getThemeFromLocalStorage() {
+		const savedTheme = localStorage.getItem("theme");
+		if (savedTheme) {
+			setTheme(savedTheme);
+		}
+	}
+
+  function toggleTheme() {
+		setTheme((prevTheme) => {
+			const newTheme = prevTheme === "light" ? "dark" : "light";
+			localStorage.setItem("theme", newTheme);
+			return newTheme;
+		});
+	}
+
+  useEffect(() => {
+    getThemeFromLocalStorage();
+    document.documentElement.setAttribute('data-theme', theme);
+  }, [theme]);
+
+  const themeIcon = theme === "light" 
+  ? <i class="bi bi-sun-fill"></i> 
+  : <i class="bi bi-moon-stars-fill"></i>
+
   return (
     <React.Fragment>
-      <nav className="navbar navbar-light bg-light">
+      <nav className={`navbar navbar-${theme} bg-${theme}`}>
         <Link to="/" style={{ textDecoration: 'none' }}>
-          <span className="navbar-brand mb-0 mx-2 h1">Money Rates</span>
+          <span className="navbar-brand mx-2 h1">Money Rates</span>
         </Link>
+        <button 
+          class={`btn btn-outline-${theme === "dark" ? "light" : "dark"} btn-sm mt-1 mx-2`} 
+          onClick={toggleTheme}
+        >
+          {themeIcon}
+        </button>
       </nav>
-      <div className="container py-3">
+      <div className="container py-3" data-bs-theme={theme}>
         {props.children}
       </div>
-      <footer className="bg-light text-center">
+      <footer className={`bg-${theme} text-center`}>
         <div className="container pt-3">
           <section className="mb-3">
             <a
-              className="btn btn-link btn-floating btn-lg text-dark m-1"
+              className={`btn btn-lg text-${theme === "dark" ? "light" : "dark"} m-1`}
               href="https://github.com/Cory117"
               target="_blank"
             >
               <i className="bi bi-github"></i>
             </a>
             <a
-              className="btn btn-link btn-floating btn-lg text-dark m-1"
+              className={`btn btn-lg text-${theme === "dark" ? "light" : "dark"} m-1`}
               href="https://www.linkedin.com/in/cory-daddona-6237a3158/"
               target="_blank"
             >
@@ -31,7 +66,10 @@ const Layout = (props) => {
             </a>
           </section>
         </div>
-        <div className="text-center text-dark p-3" style={{ backgroundColor: "#f1f1f1" }}>
+        <div 
+          className={`text-center text-${theme === "dark" ? "light" : "dark"} p-3`}
+          style={{ backgroundColor: "rgba(0, 0, 0, .1)" }}
+        >
           © 2024 Cory Daddona. All rights reserved.
         </div>
       </footer>
